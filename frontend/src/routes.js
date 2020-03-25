bbb385dbf0d0ade45055851f7a1c29e27da0b68e
Pedro Vicente <pedrovicente@poli.ufrj.br>
2020-03-25 14:31:52 -0300

    Frontend da aplicação

---
+++ frontend/src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// O Switch garante que só 1 rota é executada por vez
+
+import Logon from './pages/Logon';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Logon} />
+        <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/incidents/new" component={NewIncident} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
